3229b9fcf1b85ae99cc998bc0039049fd6c8058d
Claire Parker <claireparker@ufl.edu>
2024-10-15 19:10:18 -0400

    Add answer to problem 2

--- pset3.py
+++ pset3.py
@@ -23,3 +23,44 @@ with open("CO-OPS__8729108__wl.csv", 'r') as file:
                 highest_date_time = date_time
             
 print(f"The highest water level was {highest_water_level} ft. observed on {highest_date_time}")
+
+# Problem 2
+
+# Create variables
+highest_water_level = float('-inf')
+lowest_water_level = float('inf')
+highest_date_time = ""
+lowest_date_time = ""
+avg_water_level = 0
+water_levels_list = []
+
+with open("CO-OPS__8729108__wl.csv", 'r') as file:
+    reader = csv.reader(file)
+    header = next(reader) # Skip header row
+    
+    for row in reader:
+            date_time = row[0]
+            try:
+                water_level = float(row[1])
+            except ValueError:
+                continue
+            
+            water_levels_list.append(water_level) #Create a list of the water level values
+                
+            if water_level > highest_water_level:
+                highest_water_level = water_level
+                highest_date_time = date_time
+                
+            if water_level < lowest_water_level:
+                lowest_water_level = water_level
+                lowest_date_time = date_time
+
+# Calculate the average
+avg_water_level = sum(water_levels_list) / len(water_levels_list) if water_levels_list else None 
+
+print(f"The highest water level was {highest_water_level} ft. observed on {highest_date_time}.")
+print(f"The lowest water level was {lowest_water_level} ft. observed on {lowest_date_time}.")
+print(f"The average water level is {avg_water_level} ft.")
+                
+                
+                
