5dd5d9356594b51b92de9360bae8533fa167c830
Claire Parker <claireparker@ufl.edu>
2024-10-15 19:42:50 -0400

    Add problem 4 answer and edit some formatting

--- pset3.py
+++ pset3.py
@@ -4,8 +4,10 @@ import csv
 
 # Problem 1
 
+print("The answer to problem 1 is:")
+
 highest_water_level = float('-inf') # Create variable for water level and set it to negative infinity so any subsequent values will be larger
-highest_date_time = "" # Create variable for date and time that will correspond with the highest water level
+highest_date_time = "" # Create variable for date and time that is an empty string
 
 with open("CO-OPS__8729108__wl.csv", 'r') as file:
     reader = csv.reader(file)
@@ -23,12 +25,15 @@ with open("CO-OPS__8729108__wl.csv", 'r') as file:
                 highest_date_time = date_time
             
 print(f"The highest water level was {highest_water_level} ft. observed on {highest_date_time}")
+print()
 
 # Problem 2
 
+print("The answer to problem 2 is:")
+
 # Create variables
-highest_water_level = float('-inf')
-lowest_water_level = float('inf')
+highest_water_level = float('-inf') # Set to negative infinity
+lowest_water_level = float('inf') # Set to infinity 
 highest_date_time = ""
 lowest_date_time = ""
 avg_water_level = 0
@@ -61,11 +66,14 @@ avg_water_level = sum(water_levels_list) / len(water_levels_list) if water_level
 print(f"The highest water level was {highest_water_level} ft. observed on {highest_date_time}.")
 print(f"The lowest water level was {lowest_water_level} ft. observed on {lowest_date_time}.")
 print(f"The average water level is {avg_water_level} ft.")
+print()
 
 # Problem 3
 
+print("The answer to problem 3 is:")
+
 # Create variables
-fastest_rise = float('-inf')
+fastest_rise = float('-inf') # Set to negative infinity
 fastest_date_time = ""
 previous_water_level = None
 
@@ -92,7 +100,31 @@ with open("CO-OPS__8729108__wl.csv", 'r') as file:
         previous_water_level = water_level # Reset for the next iteration
     
 print(f"The fastest rise in water level per 6-minute period was {fastest_rise} ft. on {fastest_date_time}.")
+print()
 
+# Problem 4
 
+print("The answer to problem 4 is:")
+
+previous_water_level = None 
+
+with open("CO-OPS__8729108__wl.csv", 'r') as file:
+    reader = csv.reader(file)
+    header = next(reader)
+    
+    for row in reader:
+        
+        if water_level > 5.0:
+            print(f"Warning: The water level is over 5.0 on {row[0]}.")
+            
+        if previous_water_level is not None:
+            change = water_level - previous_water_level
+            
+            if change > 0.25:
+                print(f"Warning: The water level increase is 0.25 on {row[0]}.")
+                      
+        if len(row) < 2 or row[1] == "":
+            print(f"Warning: No reading received at {row[0]}.")
+    
                 
                 
